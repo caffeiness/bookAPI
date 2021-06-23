30083625ab3d8d8dd123f1cc1c56b8af595099b6
caffeiness <caffeine.konchan@gmail.com>
2021-06-23 23:53:57 +0900

    add health check

--- internal/http/sever.go
+++ internal/http/sever.go
@@ -1,9 +1,16 @@
 package http
 
-import "github.com/labstack/echo"
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
 
 func Run() {
 	e := echo.New()
 
+	e.GET("/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "ok")
+	})
 	e.Logger.Fatal(e.Start(":1232"))
 }
